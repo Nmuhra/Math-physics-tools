103da7a07df869d9a52aeb74ddca3b101ab49ad6
Nabegh <97016646+Nmuhra@users.noreply.github.com>
2022-02-11 07:30:37 +0200

    Add files via upload

---
+++ Fnet.py
@@ -0,0 +1,21 @@
+
+choice = input("fnet, mass, acceleration?: ").lower()
+
+if choice == "fnet":
+    Mass= int(input("mass?\n"))
+    acceleration= int(input("acceleration?\n"))
+    Fnet = (Mass*acceleration)
+    print(f"Fnet is equal to {Fnet}")
+
+if choice == "mass":
+    fnet= int(input("fnet?\n"))
+    acceleration= int(input("acceleration?\n"))
+    mass = (fnet/acceleration)
+    print(f"mass is equal to {mass}")
+
+if choice == "acceleration":
+    fnet= int(input("fnet?\n"))
+    mass= int(input("mass?\n"))
+    acceleration = (fnet/mass)
+    print(f"acceleration is equal to {acceleration}")
+
