98ff615f71dd40ac1c304259ffcd61123cc2f576
Nabegh <97016646+Nmuhra@users.noreply.github.com>
2022-02-14 07:21:50 +0200

    Update kinematics.py

--- kinematics.py
+++ kinematics.py
@@ -1,19 +1,49 @@
-variables = "distance, time, acceleration, velocity(f)"
+import dis
+from tkinter.tix import DisplayStyle
 
-print("what is the missing variable?\n"+ variables)
+while True:
+    variables = "distance, time, acceleration, velocity(f)"
+    print("what is the missing variable?\n"+ variables)
 
-variable = input()
+    variable = input()
 
-SolveD = "final, initial, acceleration, time : "
+    def square_root(x):
+        from math import sqrt
+        print(sqrt(x))
 
-if variable == "distance":
-    choice = input("solve for?\n" +SolveD)
-    if choice == "final":
-        initial = int(input("initial?\n"))
-        acceleration = int(input("acceleration?\n"))
-        time = int(input("time\n"))
-        final_velocity = initial+(acceleration*time)
-        print(f"final velocity is {final_velocity} m/s^2")
-        
+    if variable == "distance":
+       initial = int(input("initial velocity?\n"))
+       acceleration = int(input("acceleration?\n"))
+       time = int(input("time?\n"))
+       final_velocity = initial+(acceleration*time)
+       print(f"Final velocity is equal to {final_velocity} m/s^2")
 
+    if variable == "time":
+       initial = int(input("initial velocity?\n"))
+       acceleration = int(input("acceleration?\n"))
+       displacement = int(input("displacement?\n"))
+       final_velocity = int(square_root(initial**+2*acceleration+displacement))
+       print(f"Final velocity is equal to {final_velocity} m/s^2")
 
+    if variable == "acceleration":
+       final = int(input("final velocity?\n"))
+       initial = int(input("initial velocity?\n"))
+       time = int(input("time?\n"))
+       displacement = ((final+initial)/2)*time
+       print(f"Displacement will me equal to {displacement} m")
+
+    if variable == "velocity(f)":
+       initial = int(input("initial velocity?\n"))
+       time = int(input("time?\n"))
+       acceleration = int(input("acceleration?\n"))
+       displacement = initial*time + 1/2*acceleration*time*time
+       print(f"Displacement will be equal to {displacement} m")
+
+    if variable == None:
+       print("wrong input!")
+
+    again = input("again? (yes/no): ").lower()
+    
+    if again !="yes":
+        break
+print("ok bye")
